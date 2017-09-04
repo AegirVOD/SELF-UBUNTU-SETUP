28b5086a97a6150d048214c344f39a38353e5e52
Jiang Mufan <xiahouheiyin@gmail.com>
2017-09-04 22:34:54 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:hzwhuang/ss-qt5
+sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository multiverse
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt-get update
+sudo apt-get install synaptic shadowsocks-qt5 numix-icon-theme-circle flatabulous-theme steam emacs25 gnome-tweak-tool -y
